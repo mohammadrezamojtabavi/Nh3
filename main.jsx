6008cce8ac8a46ce8b9747d03a345a82ca93b937
mohammadrezamojtabavi <mohammadreza.mojtabavi@gmail.com>
2025-05-06 17:25:10 +0330

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NH3Calculator from './nh3-calculator';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NH3Calculator />
+  </React.StrictMode>
+);
